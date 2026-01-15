a6c953841f11e514fae86eefe6a761d7bd0095aa
AK <akshayramesh543@gmail.com>
2026-01-15 20:27:45 +0530

    llm integrated message feature

--- binfk_backend/test_database_logic/llmLoad.py
+++ binfk_backend/test_database_logic/llmLoad.py
@@ -1,89 +1,94 @@
-import torch
+from fastapi import FastAPI
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from transformers import BitsAndBytesConfig
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import torch
+from pydantic import BaseModel
+import uuid
 
-app = FastAPI()
+def load_model(path):
+    
+    if torch.cuda.is_available():
+        
+        fourbit_quant_config =  BitsAndBytesConfig(
+          load_in_4bit= True,
+          bnb_4bit_compute_dtype= torch.float16,
+          bnb_4bit_quant_type= "nf4",
+          bnb_4bit_use_double_quant= True
+        )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+        tokenizer = AutoTokenizer.from_pretrained(path, local_files_only =  True)
+        model = AutoModelForCausalLM. from_pretrained(
+            path,
+            torch_dtype = torch.float16,
+            device_map= 'auto',
+            quantization_config = fourbit_quant_config,
+            local_files_only =  True
+        )
 
-MODEL_PATH = "/home/akshay/Projects/models/text/models--mistralai--Mistral-7B-Instruct-v0.3/snapshots/0d4b76e1efeb5eb6f6b5e757c79870472e04bd3a"
+        pipe = pipeline(
+            'text-generation',
+            tokenizer = tokenizer,
+            model = model
+        )
 
-def llm_load_4bit(path):
-     
-     if not torch.cuda.is_available():
-          raise RuntimeError("GPU not active")
-     
-     load_4bit_config = BitsAndBytesConfig(
-          load_in_4bit = True,
-          bnb_4bit_compute_dtype = torch.float16,
-          bnb_4bit_quant_type ="nf4",
-          bnb_4bit_use_double_quant = True
-     )
+        return pipe
+    
 
-     tokenizer = AutoTokenizer.from_pretrained(
-          path,
-          local_files_only = True
-     )
 
-     model = AutoModelForCausalLM.from_pretrained(
-          path,
-          device_map = "auto",
-          torch_dtype = torch.float16,
-          quantization_config = load_4bit_config,
-          local_files_only = True
-     )
-
-     model.eval()
-
-     pipe = pipeline(
-          'text-generation',
-          model = model,
-          tokenizer = tokenizer
-     )
-
-     return pipe
-
-def text_handle(query, pipe):
-
+def query_answering(query, pipe):
+    
     prompt = f"""
-        You are a helpful coding assistant.
-        Generate me code for the following javascript of react queries.
+     
+               You are knowledge agent that gives answers for the Question efficiently
 
-        Question: {query}
+               Question: {query}
 
-        Answer:
-        """
+               Answer: 
 
+          """
     response = pipe(
         prompt,
-        max_new_tokens=4000,
-        temperature=0.3,
+        max_new_tokens=1000,
+        temperature=0.5,
         top_p=0.5
-    )[0]['generated_text']
-
-
+     )[0]['generated_text']
+    
     if "Answer:" in response:
         response = response.split("Answer:")[-1].strip()
 
     return response
 
-PIPE = llm_load_4bit(MODEL_PATH)
 
-QUERY = "Generate me a code for implementing button action in a react component which post the input to a particular endpoint " 
+MODEL_PATH = "/home/akshay/Projects/models/text/models--mistralai--Mistral-7B-Instruct-v0.3/snapshots/0d4b76e1efeb5eb6f6b5e757c79870472e04bd3a"
 
-@app.get("/chat/botresponse")
-async def chatbotAns():
-     
-     resp = text_handle(QUERY, PIPE)
 
-     return {
-          "query" : QUERY,
-          "response" : resp
-     }
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+PIPE = load_model(path= MODEL_PATH)
+
+class ChatQuery(BaseModel):
+    messageId: str
+    role: str
+    content: str
+
+@app.post("/chat/llm")
+async def get_query(query : ChatQuery):
+
+    answer = query_answering(query.content, PIPE)
+
+    return {
+        "messageId" : str(uuid.uuid4()),
+        "role" : "assistant",
+        "question": query.content,
+        "content": answer
+    }
+
+
--- binfk_console/src/app/CuesAI/components/ChatBar.js
+++ binfk_console/src/app/CuesAI/components/ChatBar.js
@@ -1,10 +1,9 @@
 "use client";
 
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from './ChatWin.module.css'
 import Image from "next/image";
-import { INFINITE_CACHE } from "next/dist/lib/constants";
 
 
 export function ChatBar() {
@@ -12,52 +11,66 @@ export function ChatBar() {
 
   const [inputMessage, setInputMessage] = useState("");
 
+  const [llmchatMessages, setLlmchatMessages] = useState([])
+
   async function InputMessagePost() {
 
     if(!inputMessage.trim()) return;
 
-    try {
+    const QueryMessage =         
+    {
+      messageId : crypto.randomUUID(),
+      role: 'user',
+      content: inputMessage.trim()
+    }
 
-      const res = await axios.post("http://localhost:4000/chat/chatInputs", {
-        "userID" : "u_001",
-        "conversationId" : crypto.randomUUID(),
-        "role" : "user",
-        "content"  : inputMessage.trim()
-      });
+    setLlmchatMessages(prev => [...prev, QueryMessage]);
+    setInputMessage("")
 
-      console.log("Message sent", res.data)
+    try{
+      const res = await axios.post("http://localhost:4040/chat/llm", QueryMessage)
+
+      const llmMessage = {
+        messageId : res.data.messageId,
+        role : "assistant",
+        content: res.data.content
+      }
+
+      setLlmchatMessages(prev => [...prev, llmMessage]);
 
     }
-    catch(error) {
-      console.error(error)
+
+    catch (error){
+
+      console.log(error);
+
     }
-    
   }
   
 
   return (
     <div className={styles.chatBar}>
         <div className={styles.chatMessages}>
-        {chatMessages.map(chat => (
-            <div key={chat.createdAt} className={chat.role == 'user' ? styles.chatUserMessage : styles}>
+        {llmchatMessages.map(chat => (
+            <div key={chat.messageId} className={chat.role === 'user' ? styles.chatUserMessage : styles.chatBotMessage}>
                 {chat.content}
             </div>
         ))}
         </div>
-
         <div className={styles.chatInputBar}>
-            <InputBar />
+            <InputBar inputMessage={inputMessage} SendIn={InputMessagePost} setInputMessage={setInputMessage} />
         </div>
     </div>
   );
 }
 
-function InputBar() {
+
+function InputBar({inputMessage,setInputMessage, SendIn}) {
   return (
     <>
       <input value={inputMessage}
         onChange={(e) => setInputMessage(e.target.value)}/>
-      <button className={styles.InputButton} onClick={InputMessagePost}>
+      <button className={styles.InputButton} onClick={SendIn}>
         <Image
           src={"/send.png"} alt="Send_Message" width={30} height={30}/>
       </button>
